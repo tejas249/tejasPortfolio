974526724a1ba2eb4217c4b9f16bf67067d1db2d
Tejas Kamble <tejaskambletk132@gmail.com>
2025-01-08 18:16:18 +0530

    Added framer-motion to Technologies section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -15,84 +15,160 @@ import {
   SiMysql, 
   SiRecoil 
 } from 'react-icons/si';
+import {motion} from 'framer-motion'
+
+const iconVariants = (duration) => ({
+    initial:{y:-10},
+    animate: {
+      y:[10,-10],
+      transition:{
+        duration:duration,
+        ease:"linear",
+        repeat:Infinity,
+        repeatType:"reverse"
+      }
+    }
+})
 
 const Technologies = () => {
   return (
     <div className='pb-24'>
-      <h2 className='my-20 text-center text-4xl text-white'>Technologies</h2>
-      <div className='flex flex-wrap items-center justify-center gap-6'>
+      <motion.h2 
+      whileInView = {{opacity:1,y:0}}
+      initial = {{opacity:0, y:-100}}
+      transition={{duration:1.5}}
+      className='my-20 text-center text-4xl text-white'>Technologies
+      </motion.h2>
+      
+      <motion.div
+       whileInView = {{opacity:1,y:0}}
+       initial = {{opacity:0, y:-100}}
+       transition={{duration:1.5}}
+       className='flex flex-wrap items-center justify-center gap-6'>
         {/* React */}
-        <div>
+        <motion.div 
+          initial="initial"
+          animate = "animate"
+          variants={iconVariants(2.5)}
+        >
           <RiReactjsLine className='text-7xl text-cyan-400' />
           <p className='text-center text-white mt-2'>React</p>
-        </div>
+        </motion.div>
         
         {/* HTML */}
-        <div>
+        <motion.div      
+          initial="initial"
+          animate = "animate"
+          variants={iconVariants(2.5)} >
           <SiHtml5 className='text-7xl text-orange-500' />
           <p className='text-center text-white mt-2'>HTML</p>
-        </div>
+        </motion.div>
         
         {/* CSS */}
-        <div>
+        <motion.div
+              initial="initial"
+              animate = "animate"
+              variants={iconVariants(2.5)}
+        >
           <SiCss3 className='text-7xl text-blue-500' />
           <p className='text-center text-white mt-2'>CSS</p>
-        </div>
+        </motion.div>
         
         {/* JavaScript */}
-        <div>
+        <motion.div
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        
+        >
           <SiJavascript className='text-7xl text-yellow-500' />
           <p className='text-center text-white mt-2'>JavaScript</p>
-        </div>
+        </motion.div>
         
         {/* NodeJS */}
-        <div>
+        <motion.div
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        
+        >
           <RiNodeTree className='text-7xl text-green-500' />
           <p className='text-center text-white mt-2'>Node.js</p>
-        </div>
+        </motion.div>
         
         {/* Express */}
-        <div>
+        <motion.div   
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        >
           <SiExpress className='text-7xl text-gray-400' />
           <p className='text-center text-white mt-2'>Express</p>
-        </div>
+        </motion.div>
         
         {/* MongoDB */}
-        <div>
+        <motion.div
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        
+        >
           <SiMongodb className='text-7xl text-green-600' />
           <p className='text-center text-white mt-2'>MongoDB</p>
-        </div>
+        </motion.div>
         
         {/* C++ */}
-        <div>
+        <motion.div   
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        
+        >
           <SiCplusplus className='text-7xl text-blue-700' />
           <p className='text-center text-white mt-2'>C++</p>
-        </div>
+        </motion.div>
         
         {/* SQL */}
-        <div>
+        <motion.div   
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        >
           <SiMysql className='text-7xl text-blue-600' />
           <p className='text-center text-white mt-2'>SQL</p>
-        </div>
+        </motion.div>
         
         {/* Tailwind */}
-        <div>
+        <motion.div   
+              initial="initial"
+              animate = "animate"
+              variants={iconVariants(2.5)}
+        >
           <SiTailwindcss className='text-7xl text-teal-400' />
           <p className='text-center text-white mt-2'>Tailwind</p>
-        </div>
+        </motion.div>
         
         {/* Bootstrap */}
-        <div>
+        <motion.div   
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        
+        >
           <SiBootstrap className='text-7xl text-purple-600' />
           <p className='text-center text-white mt-2'>Bootstrap</p>
-        </div>
+        </motion.div>
         
         {/* Recoil */}
-        <div>
+        <motion.div   
+            initial="initial"
+            animate = "animate"
+            variants={iconVariants(2.5)}
+        >
           <SiRecoil className='text-7xl text-indigo-500' />
           <p className='text-center text-white mt-2'>Recoil</p>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
