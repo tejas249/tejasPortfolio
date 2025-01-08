c70f833edd2f16235f989cfa8b7f597dfb140812
Tejas Kamble <tejaskambletk132@gmail.com>
2025-01-08 17:15:48 +0530

    Added framer-motion to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,41 +1,71 @@
 import React from 'react';
 import profilePic from "../assets/raviKumarProfile.webp";
 import { HERO_CONTENT } from "../constants";
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { 
+    opacity: 1, 
+    x: 0, 
+    transition: { duration: 0.5 } 
+  },
+};
 
 const Hero = () => {
   return (
-    <div className='pb-4 lg:mb-36 relative z-10'>
-      <div className='flex flex-wrap lg:flex-row-reverse'>
-        <div className='w-full lg:w-1/2'>
-          <div className='flex justify-center lg:p-8'>
-            <img
+    <div className="pb-4 lg:mb-36 relative z-10">
+      <div className="flex flex-wrap lg:flex-row-reverse">
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:p-8">
+            <motion.img
               src={profilePic}
-              alt=""
+              alt="Profile"
               className="border border-x-stone-900 rounded-3xl"
+              width={650}
+              height={650}
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
             />
           </div>
         </div>
-        <div className='w-full lg:w-1/2'>
-          <div className='flex flex-col items-center lg:items-start mt-10'>
-            <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>
+        <div className="w-full lg:w-1/2">
+          <motion.div initial="hidden" animate="visible" variants={containerVariants} className="flex flex-col items-center lg:items-start mt-10">
+            <motion.h2
+             variants={childVariants}
+             className="pb-2 text-4xl tracking-tighter lg:text-8xl">
               Tejas Kamble
-            </h2>
-            <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>
+            </motion.h2>
+            < motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Developer (MERN)
-            </span>
-            <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
+            </motion.span>
+            <motion.p variants={childVariants}  className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
               {HERO_CONTENT}
-            </p>
-            <a
-              className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'
-              href='resume.pdf'
-              target='_blank'
-              rel='noopener noreferrer'
+            </motion.p>
+            <motion.a
+            variants={childVariants}
+              className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
+              href="resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
               download
             >
               Download Resume
-            </a>
-          </div>
+            </motion.a> 
+          </motion.div>
         </div>
       </div>
     </div>
