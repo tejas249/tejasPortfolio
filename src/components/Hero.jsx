f29aeedabad363c9a0433db867fd250bd57ed91a
Tejas Kamble <tejaskambletk132@gmail.com>
2025-03-05 20:11:48 +0530

    Profile changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import profilePic from "../assets/tejas.jpeg";
+import profilePic from "../assets/tejas2.jpg";
 import { HERO_CONTENT } from "../constants";
 import { motion } from 'framer-motion';
 
@@ -50,7 +50,7 @@ const Hero = () => {
               Tejas Kamble
             </motion.h2>
             < motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
-              Full Stack Developer (MERN)
+              Full Stack Developer
             </motion.span>
             <motion.p variants={childVariants}  className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
               {HERO_CONTENT}
